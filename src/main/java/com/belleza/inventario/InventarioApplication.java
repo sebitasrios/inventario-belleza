b59bf634c1ad3365e062c5a07742454030e97d11
Sebastian Rios <sebasri92@gmail.com>
2026-04-06 13:37:29 -0500

    agregando swagger y configuracion final

--- src/main/java/com/belleza/inventario/InventarioApplication.java
+++ src/main/java/com/belleza/inventario/InventarioApplication.java
@@ -2,8 +2,9 @@ package com.belleza.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class InventarioApplication {
 
     public static void main(String[] args) {
