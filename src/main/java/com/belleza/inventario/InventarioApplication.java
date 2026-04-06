70467f1a54a7c543c48dca422be29a938738e89e
Sebastian Rios <sebasri92@gmail.com>
2026-04-06 12:55:53 -0500

    configuracion inicial del proyecto

--- src/main/java/com/belleza/inventario/InventarioApplication.java
+++ src/main/java/com/belleza/inventario/InventarioApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventarioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventarioApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InventarioApplication.class, args);
+    }
 }
