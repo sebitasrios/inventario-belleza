4162dd81df89b6c77129ca0bb25db16e1af878e5
Sebastian Rios <sebasri92@gmail.com>
2026-04-06 11:28:54 -0500

    primer commit - estructura del proyecto

---
+++ src/main/java/com/belleza/inventario/entities/Producto.java
@@ -0,0 +1,55 @@
+package com.belleza.inventario.entities;
+
+public class Producto {
+
+    private int id;
+    private String nombre;
+    private String categoria;
+    private double precio;
+    private int cantidad;
+
+
+    public Producto() {
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
